ed816318161b67ece0474bef3aa6a36e22de88c3
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-18 15:13:47 +0530

    add entity file and other files

---
+++ Medicine_Management_SpringBoot/src/main/java/com/example/demo/entity/Medicine.java
@@ -0,0 +1,40 @@
+package com.example.demo.entity;
+
+
+public class Medicine {
+	
+	private String mID;
+	private String mName;
+	private String mCount;
+	private String mDescription;
+	public String getmID() {
+		return mID;
+	}
+	public void setmID(String mID) {
+		this.mID = mID;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public String getmCount() {
+		return mCount;
+	}
+	public void setmCount(String mCount) {
+		this.mCount = mCount;
+	}
+	public String getmDescription() {
+		return mDescription;
+	}
+	public void setmDescription(String mDescription) {
+		this.mDescription = mDescription;
+	}
+	
+	
+	
+	
+	
+
+}
